7b947d46a9f8b7ee3cc14f859a375328083193ec
NaoyaCFDImai <imai@cfd.life>
2025-06-18 09:10:07 +0900

    UI: implement UI

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Snake;
 
@@ -16,8 +8,36 @@ namespace Snake;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly int _rows = 15, _cols = 15;
+
+    private readonly Image[,] gridImages;
+
     public MainWindow()
     {
         InitializeComponent();
+        gridImages = SetupGrid();
+    }
+
+    private Image[,] SetupGrid()
+    {
+        Image[,] images = new Image[_rows, _cols];
+        GameGrid.Rows = _rows;
+        GameGrid.Columns = _cols;
+
+        for (int row = 0; row < _rows; row++)
+        {
+            for (int col = 0; col < _cols; col++)
+            {
+                Image image = new Image
+                {
+                    Source = Images.Empty
+                };
+
+                images[row, col] = image;
+                GameGrid.Children.Add(image);
+            }
+        }
+
+        return images;
     }
 }
